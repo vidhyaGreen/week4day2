38e569751125043d08c8a8626d49555227daec48
vidhyaGreen <92054443+vidhyaGreen@users.noreply.github.com>
2021-10-24 09:25:17 -0700

    Add files via upload

---
+++ LearnForEach.java
@@ -0,0 +1,16 @@
+package Week4day2;
+
+public class LearnForEach {
+
+	public static void main(String[] args) {
+
+int arr[]= {3,4,5,6,7,8};
+for (int i : arr) {
+	
+	System.out.println(i);
+	
+}
+
+	
+}
+}
