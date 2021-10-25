dc2094b92016bb711f930b595cddc771bee48667
vidhyaGreen <92054443+vidhyaGreen@users.noreply.github.com>
2021-10-25 06:27:26 -0700

    Add files via upload

---
+++ LearnRightClick.java
@@ -0,0 +1,31 @@
+package Week4day2;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class LearnRightClick {
+
+	public static void main(String[] args) {
+		WebDriverManager.chromedriver().setup();
+		ChromeDriver driver = new ChromeDriver();
+		driver.get("http://leaftaps.com/opentaps/control/main");
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+
+    
+		WebElement header= driver.findElement(By.xpath("//label[text()='Username']"));
+		Actions Builder=new Actions(driver);
+		
+		//context clickused for double click
+		
+		Builder.contextClick().perform();
+	}
+}
+
+		
