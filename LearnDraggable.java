c2008040255fd7b82fd341ba478a92971aa3638a
vidhyaGreen <92054443+vidhyaGreen@users.noreply.github.com>
2021-10-25 06:28:43 -0700

    Add files via upload

---
+++ LearnDraggable.java
@@ -0,0 +1,31 @@
+package Week4day2;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class LearnDraggable {
+
+	public static void main(String[] args) {
+		WebDriverManager.chromedriver().setup();
+		ChromeDriver driver = new ChromeDriver();
+		driver.get("http://www.leafground.com/pages/drag.html");
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+
+    
+		WebElement drag= driver.findElement(By.id("draggable"));
+		Actions Builder=new Actions(driver);
+		Builder.dragAndDropBy(drag,200,200).perform();
+		
+		
+	
+		
+	}
+
+}
